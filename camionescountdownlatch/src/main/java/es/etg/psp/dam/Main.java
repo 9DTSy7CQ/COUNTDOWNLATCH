528d2b4f6114cefe9cbde062a3fea045782d8d83
AlexROD <alconralf3@gmail.com>
2025-11-28 16:12:44 +0100

    Comentarios commit

--- camionescountdownlatch/src/main/java/es/etg/psp/dam/Main.java
+++ camionescountdownlatch/src/main/java/es/etg/psp/dam/Main.java
@@ -9,25 +9,22 @@ public class Main{
 
     public static void main(String[] args) throws InterruptedException {
 
-        CountDownLatch cuenta = new CountDownLatch(5); // Esperar 5 camiones
+        CountDownLatch cuenta = new CountDownLatch(5); 
 
         System.out.println(INICIO_ESPERA);
 
-        // Crear los hilos
         Thread c1 = new Thread(new Camion("Camion-1", cuenta));
         Thread c2 = new Thread(new Camion("Camion-2", cuenta));
         Thread c3 = new Thread(new Camion("Camion-3", cuenta));
         Thread c4 = new Thread(new Camion("Camion-4", cuenta));
         Thread c5 = new Thread(new Camion("Camion-5", cuenta));
 
-        // Iniciar los hilos
         c1.start();
         c2.start();
         c3.start();
         c4.start();
         c5.start();
 
-        // Esperar a que todos lleguen
         cuenta.await();
 
         System.out.println("\n" + TODOS_LLEGADOS);
